c2fcf9e6d5edea174f0cc48a9501fd2d998080cc
peaceandwhisky <pioneerandf@gmail.com>
2024-11-17 05:38:10 +0700

    add flowTestnet

--- vlayer-react/src/constants.ts
+++ vlayer-react/src/constants.ts
@@ -1,6 +1,6 @@
-import { sepolia, optimismSepolia, baseSepolia, polygonZkEvmTestnet } from "viem/chains";
+import { sepolia, optimismSepolia, baseSepolia, flowTestnet, polygonZkEvmTestnet } from "viem/chains";
 
-export const CHAINS = [sepolia, optimismSepolia, baseSepolia, polygonZkEvmTestnet] as const;
+export const CHAINS = [sepolia, optimismSepolia, baseSepolia, flowTestnet, polygonZkEvmTestnet] as const;
 
 export type ChainId = (typeof CHAINS)[number]["id"];
 
@@ -11,6 +11,8 @@ export const CHAIN_IMAGES: Record<ChainId, string> = {
     "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/optimism/info/logo.png",
   [baseSepolia.id]:
     "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/base/info/logo.png",
+  [flowTestnet.id]:
+    "",
   [polygonZkEvmTestnet.id]:
     "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/polygonzkEvm/info/logo.png",
 } satisfies Record<ChainId, string>;
