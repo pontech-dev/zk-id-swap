c3447ed88ef8db7f212fdee93831c6268e75104e
peaceandwhisky <pioneerandf@gmail.com>
2024-11-17 04:45:08 +0700

    add baseSepolia

--- vlayer-react/src/constants.ts
+++ vlayer-react/src/constants.ts
@@ -1,14 +1,16 @@
-import { sepolia, optimismSepolia, polygonZkEvmTestnet } from "viem/chains";
+import { sepolia, optimismSepolia, baseSepolia, polygonZkEvmTestnet } from "viem/chains";
 
-export const CHAINS = [sepolia, optimismSepolia, polygonZkEvmTestnet] as const;
+export const CHAINS = [sepolia, optimismSepolia, baseSepolia, polygonZkEvmTestnet] as const;
 
 export type ChainId = (typeof CHAINS)[number]["id"];
 
 export const CHAIN_IMAGES: Record<ChainId, string> = {
   [sepolia.id]:
-    "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/sepolia/info/logo.png",
+    "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png",
   [optimismSepolia.id]:
     "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/optimism/info/logo.png",
+  [baseSepolia.id]:
+    "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/base/info/logo.png",
   [polygonZkEvmTestnet.id]:
     "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/polygonzkEvm/info/logo.png",
 } satisfies Record<ChainId, string>;
